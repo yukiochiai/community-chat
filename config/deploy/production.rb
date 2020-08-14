11a50401611f70a68f0698e05921758803f6c274
yukiochiai <ochiai.y41@gmail.com>
2020-08-14 10:34:44 +0900

    elastic_ip修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server 'http://54.150.13.58/', user: 'ec2-user', roles: %w{app db web}
+server '54.150.13.58', user: 'ec2-user', roles: %w{app db web}
