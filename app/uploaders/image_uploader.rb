09632e270c67db238d748f07074cb75a81657a58
yukiochiai <ochiai.y41@gmail.com>
2020-08-14 18:25:46 +0900

    capistrano関連

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
   process resize_to_fill: [300, 200]
   # storage :fog
   # Override the directory where uploaded files will be stored.
