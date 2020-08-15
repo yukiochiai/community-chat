7c97b8f55120e0c75a2fea7a64bc9f3c7100cae1
yukiochiai <ochiai.y41@gmail.com>
2020-08-15 14:06:25 +0900

    下部修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'asakusainfo'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/asakusainfo'
+  config.asset_host = 'https://asakusainfo.s3.amazonaws.com'
 end
